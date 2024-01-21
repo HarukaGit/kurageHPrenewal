5cf097eecfef4d879f5c53103812fed61a8e6a2a
harukakato <>
2024-01-21 10:01:42 +0900

    フォント調整

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 'use strict'
+
+// fontロード用
+setTimeout(function() {
+  if (document.getElementsByTagName("html")[0].classList.contains('wf-active') != true) {
+      document.getElementsByTagName("html")[0].classList.add('loading-delay');
+  }
+}, 3000);
+
 // ハンバーガーメニューアニメーション
 $(".openbtn").click(function () {
   $(this).toggleClass('active');
